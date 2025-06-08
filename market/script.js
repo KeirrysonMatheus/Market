f7fc7c85d4f77630aba4317bcc79cb85b8c86597
KeirrysonMatheus <keirrysonmatheusrodriguez@gmail.com>
2025-06-08 00:03:51 -0300

    Update script.js

--- market/script.js
+++ market/script.js
@@ -1,5 +1,6 @@
 const user = JSON.parse(localStorage.getItem("user"))  
 const accountName = document.getElementById('accountName')
 if(user){
+accountName.innerHTML = ''
 accountName.innerHTML = `<span id="accountName">${user.Name}</span>`
 }
